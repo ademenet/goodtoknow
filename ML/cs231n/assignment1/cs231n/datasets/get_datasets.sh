1ab50359f5816ad6ee6b3ff5186ca48ec6c5cbf2
Alain DEMENET <ademenet@e2r8p16.42.fr>
2017-12-12 14:38:40 +0100

    Start cs231n course

---
+++ ML/cs231n/assignment1/cs231n/datasets/get_datasets.sh
@@ -0,0 +1,4 @@
+# Get CIFAR10
+curl -o /Users/ademenet/goinfre/dataset/cifar-10.tar.gz http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xzvf /Users/ademenet/goinfre/dataset/cifar-10.tar.gz
+rm /Users/ademenet/goinfre/dataset/cifar-10.tar.gz 
